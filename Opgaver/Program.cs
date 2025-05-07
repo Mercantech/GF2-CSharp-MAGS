d4b3184e60b6315b8420b56b26ec7dcb14027755
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-05-07 07:45:05 +0200

    Update Program.cs

--- Opgaver/Program.cs
+++ Opgaver/Program.cs
@@ -1,17 +1,39 @@
-﻿namespace Opgaver
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿switch (valg)
+                {
+                    case "1":
+                        Variabler.Run();
+                        break;
+                    case "2":
+                        Inputs.Run();
+                        break;
+                    case "3":
+                        ControlFlow.Run();
+                        break;
+                    case "4":
+                        Loops.Run();
+                        break;
+                    case "5":
+                        Arrays.Run();
+                        break;
+                    case "6":
+                        Methods.Run();
+                        break;
+                    case "7":
+                        Classes.Run();
+                        break;
+                    case "8":
+                        RockPaperScissors.Run();
+                        break;
+                    case "!":
+                        run = false;
+                        Console.WriteLine("Programmet afsluttes. Tak for denne gang!");
+                        break;
+                    case "?":
+                        Hjælp();
+                        break;
+                    default:
+                        Console.WriteLine("Ugyldigt valg!");
+                        Console.Write("Indtast dit valg: ");
 
-
-            //Kør Variabler Opgaverne!
-            Variabler.Run();
-            Indputs.Run();
-        }
-
-        
-    }
-}
+                        break;
+                }
